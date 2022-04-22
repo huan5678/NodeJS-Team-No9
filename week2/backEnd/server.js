3b7c55179750b9affc16e9a432ca0e3491ddf1a2
konstante <konstantechang@gmail.com>
2022-04-22 18:42:32 +0800

    work on  server.js

--- week2/backEnd/server.js
+++ week2/backEnd/server.js
@@ -1,3 +1,7 @@
 const http = require('http');
 const mongoose = require('mongoose');
 const {success, error } = require('./utils/handlers');
+require('dotenv').config()
+
+console.log(PORT);
+console.log('hello');
