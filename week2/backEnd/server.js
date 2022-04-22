e8cf5699cda70ea3d6fa80b0608daf774df93790
konstante <konstantechang@gmail.com>
2022-04-22 23:03:54 +0800

    modified server.js  add GET method and POST method (...ing)

--- week2/backEnd/server.js
+++ week2/backEnd/server.js
@@ -1,7 +1,28 @@
 const http = require('http');
 const mongoose = require('mongoose');
 const {success, error } = require('./utils/handlers');
-dotenv.config({path:'./config.env'})
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({path:"./config.env"});
+const PostsModel = require('./models/Post');
 
-console.log(PORT);
-console.log('hello');
+// console.log(process.env.PORT);
+// console.log('hello');
+
+mongoose.connect('mongodb://localhost:27017/Post').then(
+     () => { console.log("己連接資料庫!!") } ).catch(
+          error => {console.log(error)} );
+
+const requestListener = async (req, res) => {
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk;
+    });
+
+    if( (req.url == '/' || req.url == '/posts') && req.method == 'GET' ){
+        const allPost = PostsModel.find();
+        success(res, allPost);
+    }else if(req.url == '/posts' && req.method == 'POST'){
+
+    };//edn of POST method
+};
